89ba50360c254ea31f2b91243652bd94bf4a075a
Taliavivrett <taliavivrett@gmail.com>
2024-12-24 13:00:28 -0800

    feat: added blog.ts and blog.js

---
+++ src/blog.ts
@@ -0,0 +1,32 @@
+// Blog interface
+export interface Blog {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageALT: string;
+    slug: string;
+}
+
+// Array
+const blogs: Blog[] = [
+  {
+    title: "Travel",
+    date: "Summer 2024",
+    description: "Places I've been this past summer",
+    image: "/images/Camping.jpg", 
+    imageALT: "Picture of a forest",
+    slug: "travel-summer-2024",
+  },
+  {
+    title: "Food",
+    date: "Summer 2024",
+    description: "My favorite meals that I've eaten/cooked this past summer",
+    image: "/images/Food.jpg", 
+    imageALT: "Picture of a meal I made",
+    slug: "food-summer-2024",
+  },
+];
+
+// Export blogs
+export default blogs;
