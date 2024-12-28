2fbee3159daea67e794033417a90ad7766151c9b
Taliavivrett <taliavivrett@gmail.com>
2024-12-27 22:26:10 -0800

    feat: updated blog.js

--- src/blog.js
+++ src/blog.js
@@ -3,7 +3,7 @@ var blogs = [
         title: "Travel",
         date: "Summer 2024",
         description: "Places I've been this past summer",
-        image: "Camping.jpg",
+        image: "../Camping.jpg",
         imageALT: "Picture of a forest",
         slug: "travel-summer-2024",
     },
@@ -11,7 +11,7 @@ var blogs = [
         title: "Food",
         date: "Summer 2024",
         description: "My favorite meals that I've eaten/cooked this past summer",
-        image: "Food.jpg",
+        image: "../Food.jpg",
         imageALT: "Picture of a meal I made",
         slug: "food-summer-2024",
     },
@@ -50,4 +50,6 @@ function appendingBlog() {
     }
 }
 
-appendingBlog();
+document.addEventListener('DOMContentLoaded', function() {
+    appendingBlog();
+});
