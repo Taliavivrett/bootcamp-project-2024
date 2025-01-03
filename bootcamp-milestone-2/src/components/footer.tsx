eb164eafbfef7fa6fc8919b4410be97f09e1419c
Taliavivrett <taliavivrett@gmail.com>
2025-01-02 23:52:25 -0800

    feat: fixed footer

--- bootcamp-milestone-2/src/components/footer.tsx
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -3,7 +3,9 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>2024 &lt;Talia Vivrett&gt; | All Rights Reserved</p>
+      <p>2024 {'| '}Talia Vivrett{' |'} All Rights Reserved</p>
     </footer>
   );
 }
+
+
